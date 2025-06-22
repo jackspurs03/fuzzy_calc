9093a3443bc7633eeb36111d25ea8000c39e4774
Егор Печинский <pechinskyegor@gmail.com>
2025-06-23 00:59:46 +0300

    а если так

--- src/main.c
+++ src/main.c
@@ -5,25 +5,50 @@ int main(int argc, char *argv[]) {
     system ("chcp 65001>null");
 //потом вывод строки поменяем на сами операции 
 if (strcmp(argv[1], "and") == 0 || strcmp(argv[1], "AND") == 0) {
-        printf("Будет выполняться операция И\n");
+     if(argc>4) printf("Слишком много файлов");
+        else{
+            for(int i=2; i<argc; i++){
+                FILE *file;
+                file=fopen(argv[i],"r");
+        if (file == NULL) {
+            printf("Файл %s не найден\n", argv[i]);
+        }
+        fclose(file);
+               
+            }
+        }
+
     }
+    
     else if (strcmp(argv[1], "or") == 0 || strcmp(argv[1], "OR") == 0) {
-        printf("Будет выполняться операция ИЛИ\n");
+       if(argc>4) printf("Слишком много файлов");
+        else{
+            for(int i=2; i<argc; i++){
+                FILE *file;
+                file=fopen(argv[i],"r");
+        if (file == NULL) {
+            printf("Файл %s не найден\n", argv[i]);
+        }
+        fclose(file);
+               
+            }
+        }
     }
+    
     else if (strcmp(argv[1], "not") == 0 || strcmp(argv[1], "NOT") == 0) {
-        printf("Будет выполняться операция НЕ\n");
+        if(argc>3 ) printf("Слишком большое кол-во файлов\n");
+        else {
+        FILE *file;
+        file=fopen(argv[3],"r");
+        if (file == NULL) {
+            printf("Файл %s не найден\n", argv[2]);
+        }
+        fclose(file);
+        }
     }
+    
     else {
         printf("Введена неверная операция\n");
     }
-for(int i=2;i<argc;i++){
-  FILE *file;
-  file=fopen(argv[i],"r");
-  if (file == NULL) {
-        printf("Файл %s не найден\n",argv[i]); // Выводим системное сообщение об ошибке
-        continue;
-    }
-  fclose(file);
-}
     return 0;
 }

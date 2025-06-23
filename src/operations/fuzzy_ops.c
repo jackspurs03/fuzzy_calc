4717b9c478e77cc91fa08bd589eec87590b5d651
jackspurs03 <jackspurs03@yandex.ru>
2025-06-23 18:59:32 +0300

    feat: операция AND

---
+++ src/operations/fuzzy_ops.c
@@ -0,0 +1,31 @@
+#include "../data/hasht.h"
+
+// Объединение двух множеств
+HashTable* AND(HashTable* tableA, HashTable* tableB) {
+    HashTable* result = create_table();
+
+    // Обрабатываем первое множество
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        HashNode* node = tableA->buckets[i];
+        while (node != NULL) {
+            float b_val = find(tableB, node->key);
+            float max_val = (node->value > b_val) ? node->value : b_val;
+            upsert(result, node->key, max_val);
+            node = node->next;
+        }
+    }
+
+    // Добавляем уникальные элементы из второго множества
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        HashNode* node = tableB->buckets[i];
+        while (node != NULL) {
+            // Если элемент уже обработан в первом цикле, пропускаем
+            if (find(result, node->key) == 0.0f) {
+                upsert(result, node->key, node->value);
+            }
+            node = node->next;
+        }
+    }
+
+    return result;
+}

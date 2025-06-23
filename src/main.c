10c09cb0f102e30bfbaea380c4da9271d757b244
jackspurs03 <jackspurs03@yandex.ru>
2025-06-23 19:00:21 +0300

    feat: пример использования

--- src/main.c
+++ src/main.c
@@ -59,3 +59,50 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
+
+
+
+/* ПРИМЕР ИСПОЛЬЗОВАНИЯ
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "./data/hasht.h"
+#include "./io/file_io.h"
+#include "./operations/fuzzy_ops.h"
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        printf("Usage: %s <file1> <file2> <output>\n", argv[0]);
+        return 1;
+    }
+
+    HashTable* setA = read_set_from_file(argv[1]);
+    HashTable* setB = read_set_from_file(argv[2]);
+    
+    if (!setA || !setB) {
+        fprintf(stderr, "Error reading input files\n");
+        if (setA) free_table(setA);
+        if (setB) free_table(setB);
+        return 1;
+    }
+
+    printf("Sets loaded: A=%d elements, B=%d elements\n", setA->count, setB->count);
+    
+    HashTable* result = AND(setA, setB);
+    write_set_to_file(result, argv[3]);
+
+    printf("Union completed. Result: %d elements\n", result->count);
+    
+    // Освобождение памяти
+    free_table(setA);
+    free_table(setB);
+    free_table(result);
+
+    return 0;
+}
+
+*/
